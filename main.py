38e295846c0ea168d423ea3e6bd5beae3f62a51c
Walter Thumann <wthumann@gmail.com>
2025-04-08 20:03:25 -0400

    level up

--- main.py
+++ main.py
@@ -1,47 +1,54 @@
 import random
 from player import Personaje
 
+
 def main():
+    counter = 0
+
     print("escribe tu nombre")
     nombre = input("> ")
     player = Personaje(nombre)
     while player.alive:
         print(player)
-       
-        enemigo = Personaje("enemigo")
-        enemigo.hp = random.randint(3,7)
-        enemigo.str = random.randint(1,5)
-        enemigo.defense = random.randint(0,2)
 
-        counter = 0
-       
+        enemigo = Personaje(
+            "enemigo", random.randint(3, 7), random.randint(1, 5), random.randint(0, 2)
+        )
+        enemigo.exp = enemigo.hp + enemigo.str + enemigo.defense
+
         print(enemigo)
-        while(enemigo.hp > 0 and player.hp > 0):
-            print("1. atacar")
-            print("2. escapar")
-            n = input("> ")
+        while enemigo.hp > 0 and player.hp > 0:
+            while True:
+                print("1. atacar")
+                print("2. escapar")
+                n = input("> ")
+                if n in ["1", "2"]:
+                    break
             if n == "1":
                 player.attack(enemigo)
                 if enemigo.hp > 0:
                     enemigo.attack(player)
+                else:
+                    print(f"{enemigo.name} ha muerto")
+                    counter += 1
+                    player.exp += enemigo.exp
+                    if player.exp >= player.next_level:
+                        print(f"{player.name} ha subido de nivel!")
+                        player.level_up()
+                    break
             elif n == "2":
-                if random.randint(1,5) == 5:
+                if random.randint(1, 5) == 5:
                     print(f"{player.name} escapó")
+                    break
                 else:
                     print(f"{player.name} no logra escapar!")
                     enemigo.attack(player)
-            else:
-                enemigo.attack(player)
+
             if player.hp <= 0:
                 print(f"{player.name} ha muerto")
                 print(f"{player.name} logró vencer a {counter} enemigos")
                 player.alive = False
                 exit
-            if enemigo.hp <= 0:
-                print(f"{enemigo.name} ha muerto")
-                counter += 1
-                break
-       
-   
+
 
 main()
--- player.py
+++ player.py
@@ -1,11 +1,14 @@
 class Personaje:
 
-    def __init__(self, name):
+    def __init__(self, name, hp=10, str=3, defense=1, exp=0):
         self.name = name
         self.alive = True
-        self.hp = 10
-        self.str = 3
-        self.defense = 1
+        self.hp = hp
+        self.max_hp = hp
+        self.str = str
+        self.defense = defense
+        self.exp = exp
+        self.next_level = 30
 
     def __str__(self):
         return f"{self.name}: salud={self.hp}, ataque={self.str}, defensa={self.defense}"
@@ -14,3 +17,10 @@ class Personaje:
         dmg = self.str - objetivo.defense
         print(f"{self.name} ataca a {objetivo.name} y le hace {dmg} puntos de daño")
         objetivo.hp -= dmg
+
+    def level_up(self):
+        self.max_hp = int(self.max_hp * 1.15)
+        self.hp = self.max_hp
+        self.str += max(int(self.str * 1.25), 1)
+        self.defense += max(int(self.defense * 1.2), 1)
+        self.next_level = int(self.next_level * 2.2)
